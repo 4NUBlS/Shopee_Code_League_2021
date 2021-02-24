643f31ba50a0219ea185322c0c5756c725ac7af1
4NUBlS <xexjz0leechersteamofficialclub@gmail.com>
2021-02-24 15:56:17 +0700

    update

--- Python_fundamental_for_Data_Science_by_Botnoi/main.py
+++ Python_fundamental_for_Data_Science_by_Botnoi/main.py
@@ -2,3 +2,17 @@
 a = ["บ้าน", "วันนี้", "คน", "อยู่", "เดียว", "ผม"]
 print(a[1] + a[5] + a[3] + a[0] + a[2] + a[4])
 print("="*30)
+
+# Q&A_2
+def calculategrade(myscore):
+    if myscore >= 80:
+        grade = "A"
+    elif myscore >= 70:
+        grade = "B"
+    elif myscore >= 60:
+        grade = "C"
+    elif myscore >= 50:
+        grade = "D"
+    elif myscore < 50:
+        grade = "F"
+    return grade
